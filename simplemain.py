f31b64bb1c2bfcbd0b36901d56b1456b84e5aca5
Sarah Schwarz <schwarz@ucsb.edu>
2024-09-08 12:33:45 -0700

    Print directory contents

--- simplemain.py
+++ simplemain.py
@@ -18,6 +18,7 @@ VBS   = np.zeros_like(VAS)     # Zeros on CH2
 FIELD = (-90_000, 90_000, 50)  # -90k Oe to 90k Oe at 10 Oe/sec?
 
 QD_FILES = r"C:/Users/sysadmin/Desktop/Razobill-WilsonGroup/Sarah/*.dat"
+print(os.listdir(QD_FILES))
 QD_FILE = max(glob.glob(QD_FILES), key=os.path.getctime)
 print("Using File:", QD_FILE)
 
