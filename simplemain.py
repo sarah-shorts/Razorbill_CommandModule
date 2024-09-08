eacf0e02dc141f442f765a384f1bc62f0e9f0b30
Sarah Schwarz <schwarz@ucsb.edu>
2024-09-08 12:36:39 -0700

    blah

--- simplemain.py
+++ simplemain.py
@@ -17,9 +17,9 @@ VAS   = np.array([0,5,10,15,20]) # Voltages on CH1 Tension MAX V is 50V
 VBS   = np.zeros_like(VAS)     # Zeros on CH2
 FIELD = (-90_000, 90_000, 50)  # -90k Oe to 90k Oe at 10 Oe/sec?
 
-QD_FILES = r"C:/Users/sysadmin/Desktop/Razobill-WilsonGroup/Sarah/*.dat"
+QD_FILES = r"C:/Users/sysadmin/Desktop/Razobill-WilsonGroup/Sarah/"
 print(os.listdir(QD_FILES))
-QD_FILE = max(glob.glob(QD_FILES), key=os.path.getctime)
+QD_FILE = max(glob.glob(QD_FILES+"*.dat"), key=os.path.getctime)
 print("Using File:", QD_FILE)
 
 assert len(VAS) == len(VBS)
