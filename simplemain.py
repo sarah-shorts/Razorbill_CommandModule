4b31d0de750463e8234c51d234f17f51e1c1b4c6
Sarah Schwarz <schwarz@ucsb.edu>
2024-09-08 12:19:59 -0700

    update defaults

--- simplemain.py
+++ simplemain.py
@@ -1,26 +1,37 @@
 import time
 import pickle
+import os
+import glob
 
 import numpy as np
 
 from dataclasses import dataclass
 from QDInst import QDInstrument
-from pyvisa import ResurceManager
+from pyvisa import ResourceManager
 
 
-TRANSITION_TEMP = 50           # Warm to this with zero field, zero voltage
+TRANSITION_TEMP = 30           # Warm to this with zero field, zero voltage
 
-TEMPS = [30, 15, 10, 5, 2] # Just one temp, 30K
+TEMPS = [30, 10, 5, 2] # Just one temp, 30K
 VAS   = np.array([0,5,10,15,20]) # Voltages on CH1 Tension MAX V is 50V
 VBS   = np.zeros_like(VAS)     # Zeros on CH2
-FIELD = (-90_000, 90_000, 10)  # -90k Oe to 90k Oe at 10 Oe/sec?
+FIELD = (-90_000, 90_000, 50)  # -90k Oe to 90k Oe at 10 Oe/sec?
+
+QD_FILES = r"C:\Users\sysadmin\desktop\Razobill-WilsonGroup\Sarah\*.dat"
+QD_FILE = max(glob.glob(QD_FILES), key=os.path.getctime)
+print("Using File:", QD_FILE)
 
 assert len(VAS) == len(VBS)
 
-rm = ResurceManager()
+rm = ResourceManager()
+
+#use check resources to confirm the port ID of each intrument (USB port ID for the Razorbill ~sparky~ is likely to change)
+sparky_port = "ASRL10::INSTR"
+andy_port = "GPIB::28::INSTR" 
+
 
 class Sparky:
-    def __init__(self, label="ASRL10::INSTR"):
+    def __init__(self, label=sparky_port):
         self.sparky = rm.open_resource(label)
 
     def ch1(self, voltage):
@@ -42,7 +53,7 @@ class Sparky:
 
 
 class Andy:
-    def __init__(self, label="GPIB::28::INSTR"):
+    def __init__(self, label=andy_port):
         self.andy = rm.open_resource(label)
 
     def capacitance_string(self):
@@ -118,6 +129,7 @@ class Measurment:
     voltages: tuple[float, float]
     field: float
     capstring: str
+    qdline: tuple[str, int]
 
 
 measurments = []
@@ -134,17 +146,20 @@ for temp in TEMPS:
         sparky.ch1(va)
         sparky.ch2(vb)
 
+        pickle.dump((measurments, open(QD_FILE, "r").read()) , open("backup-{:f}.pkl".format(time.time()), "wb"))
+
         qd.ramp_field(*FIELD)
         while not qd.ramp_complete():
+            lines = open(QD_FILE, 'r').readlines()
             measurments.append(
                 Measurment(
                     temp,
                     (va, vb),
                     qd.get_field(),
                     andy.capacitance_string(),
+                    (lines[-1], len(lines))
                 )
             )
-            print(measurements[-1])
+            print(measurments[-1])
 
-
-pickle.dump(measurments , open("mymeasurements-{:f}.pkl".format(time.time()), "wb"))
+pickle.dump((measurments, open(QD_FILE, "r").read()) , open("mymeasurements-{:f}.pkl".format(time.time()), "wb"))
